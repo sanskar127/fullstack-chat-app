36809588f223812278e4d51936290ba668900f46
Sanskar Pandey <sanskarpandey281@gmail.com>
2024-03-29 17:54:59 +0530

    CORS Middleware removed, missing user route and user controller written

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from "cors"
 import messageRoutes from "./routes/message.routes.js"
 import connectionDB from "./database/connectionDB.js"
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 // Initializing Express App
 const App = express()
@@ -13,7 +14,7 @@ const App = express()
 // Express middleware
 App.use(express.json()) // to parse incoming requests with json payloads
 App.use(cookieParser()) // CookieParser
-App.use(cors()) // CORS Policy
+// App.use(cors()) // CORS Policy
 
 dotenv.config()
 // Environmental Variables
@@ -22,6 +23,7 @@ const PORT = process.env.PORT
 // Routes
 App.use('/api/auth', authRoutes)
 App.use('/api/message', messageRoutes)
+App.use("/api/users", userRoutes)
 App.post('/api/test', (req, res) => {
     const { message } = req.body
     res.json({ message: message})
