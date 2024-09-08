06530900c9a55609cb7a7e1f5d1d57dbd62114d3
Sanskar Pandey <sanskarpandey281@gmail.com>
2024-09-09 02:30:01 +0530

    Changes: connectionDB-mongoose syntax changed;

--- src/database/connectionDB.js
+++ src/database/connectionDB.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose"
 
 const connectionDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI + "/chatapp")
-        console.log("Connected to DB Successfully")
-    } catch (error) {
-        console.log(error.message)
-    }
+    await mongoose.connect(process.env.MONGODB_URI + "/chatapp")
+    .then(() => console.log("Connected to DB Successfully"))
+    .catch(err => console.error('MongoDB connection error:', err))
+    
 }
 
 export default connectionDB
