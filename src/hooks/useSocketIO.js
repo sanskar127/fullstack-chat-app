5782e61ef96b887f8d46b7417ef326adae6dfdc0
Sanskar Pandey <sanskarpandey281@gmail.com>
2024-09-09 01:35:13 +0530

    finding issue with why messages are not sending in realtime

--- src/hooks/useSocketIO.js
+++ src/hooks/useSocketIO.js
@@ -1,52 +1,45 @@
-import { useEffect } from "react";
-import { setOnlineUsers, setSocket } from "../features/Socket/socketSlice";
-import { io } from "socket.io-client";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react"
+import { setOnlineUsers, setSocket } from "../features/Socket/socketSlice"
+import { io } from "socket.io-client"
+import { useDispatch, useSelector } from "react-redux"
 
-const useSocketIO = (authUser) => {
-    const dispatch = useDispatch();
-    const socket = useSelector((state) => state.socket.socket);
+const useSocketIO = authUser => {
+    const dispatch = useDispatch()
+    const socket = useSelector(state => state.socket.socket)
 
     useEffect(() => {
-        let mySocket;
-
         if (authUser) {
             // Initialize socket connection
-            mySocket = io("http://localhost:3000", {
+            const mySocket = io("http://localhost:3000", {
                 query: {
                     userId: authUser._id,
                 },
-            });
-
-            console.log(mySocket)
+            })
 
             // Dispatch socket instance to Redux store
-            dispatch(setSocket(mySocket));
+            dispatch(setSocket(mySocket))
 
             // Listen for 'getUsersOnline' event
-            mySocket.on("getOnlineUsers", (users) => {
-                console.log(users)
-                dispatch(setOnlineUsers(users));
-            });
+            mySocket.on("getOnlineUsers", users => dispatch(setOnlineUsers(users)))
 
             // Cleanup on component unmount or authUser change
             return () => {
                 if (mySocket) {
-                    mySocket.off("getUsersOnline"); // Remove listeners
-                    mySocket.close(); // Close connection
-                    dispatch(setSocket(null)); // Optionally clear socket from Redux store
+                    mySocket.off("getUsersOnline") // Remove listeners
+                    mySocket.close() // Close connection
+                    dispatch(setSocket(null)) // Optionally clear socket from Redux store
                 }
-            };
+            }
         } else {
             // Handle case where authUser is null
             if (socket) {
-                socket.close();
-                dispatch(setSocket(null));
+                socket.close()
+                dispatch(setSocket(null))
             }
         }
 
-    }, [authUser, dispatch]);
+    }, [authUser, dispatch])
 
-};
+}
 
-export default useSocketIO;
+export default useSocketIO
