fdc2870c5826ed06af96d673da0d9022630468d1
Sanskar Pandey <sanskarpandey281@gmail.com>
2024-09-10 00:09:58 +0530

    Console Output

--- src/database/connectionDB.js
+++ src/database/connectionDB.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose"
 
 const connectionDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI + "/chatapp")
-    .then(() => console.log("Connected to DB Successfully"))
+    .then(() => console.log("Connected to DB Successfully ", process.env.MONGODB_URI, " on port:", process.env.PORT ))
     .catch(err => console.error('MongoDB connection error:', err))
-    
 }
 
 export default connectionDB
